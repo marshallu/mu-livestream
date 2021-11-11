c3a014f8503d6c34d4245b4135474beea7fb907e
Christopher McComas <cmccomas@marshall.edu>
2021-11-11 13:16:15 -0500

    wip

--- shortcodes.php
+++ shortcodes.php
@@ -9,6 +9,82 @@ require WP_PLUGIN_DIR . '/mu-livestream/vendor/autoload.php';
 
 use Carbon\Carbon;
 
+
+/**
+ * Shortcode to display upcoming livestreams
+ *
+ * @param array  $atts Shortcode attributes.
+ * @param string $content Shortcode content.
+ *
+ * @return string Shortcode output.
+ */
+function mu_livestream_live( $atts, $content = null ) {
+	$data = shortcode_atts(
+		array(
+			'ids'     => false,
+			'title'   => 'Live Now',
+			'channel' => false,
+		),
+		$atts
+	);
+
+	$livestream_query = new WP_Query(
+		array(
+			'post_type'      => 'mu-livestream',
+			'posts_per_page' => 12,
+			'meta_key'       => 'mu_livestream_start', // phpcs:ignore
+			'orderby'        => 'meta_value',
+			'meta_type'      => 'DATETIME',
+			'order'          => 'ASC',
+			'meta_query'     => array( // phpcs:ignore
+				'relation' => 'AND',
+				array(
+					'key'     => 'mu_livestream_end',
+					'value'   => Carbon::now()->setTimezone( 'America/Detroit' )->format( 'Y-m-d H:i:s' ), // phpcs:ignore
+					'type'    => 'DATETIME',
+					'compare' => '>=',
+				),
+				array(
+					'key'     => 'mu_livestream_start',
+					'value'   => Carbon::now()->setTimezone( 'America/Detroit' )->format( 'Y-m-d H:i:s' ), // phpcs:ignore
+					'type'    => 'DATETIME',
+					'compare' => '<=',
+				),
+				array(
+					'key'     => 'mu_livestream_live_event_id',
+					'compare' => 'EXISTS',
+				),
+			),
+		)
+	);
+
+	$html = '<div>';
+	if ( $livestream_query->have_posts() ) {
+		$html .= '<h2>' . esc_attr( $data['title'] ) . '</h2>';
+		$html .= '<div class="flex flex-wrap lg:-mx-6">';
+
+		while ( $livestream_query->have_posts() ) {
+			$livestream_query->the_post();
+			$html .= '<div class="w-full lg:w-1/3 lg:px-6 mb-6 flex">';
+			$html .= '<div>';
+			$html .= '<a href="https://vimeo.com/event/' . esc_attr( get_field( 'mu_livestream_live_event_id', get_the_ID() ) ) . '" class="text-gray-700 group no-underline">';
+			$html .= '<img src="' . esc_url( get_field( 'mu_livestream_thumbnail', get_the_ID() )['url'] ) . '" class="rounded-t" />';
+			$html .= '<div class="bg-gray-100 px-6 py-4 rounded-b">';
+			$html .= '<div class="text-xl font-semibold group-hover:underline">' . esc_attr( get_the_title() ) . '</div>';
+			$html .= '<div class="text-sm uppercase font-medium mt-1">' . esc_attr( Carbon::parse( get_field( 'mu_livestream_start', get_the_ID() ) )->format( 'l, F j, Y g:i A' ) ) . '</div>';
+			$html .= '</div>';
+			$html .= '</a>';
+			$html .= '</div>';
+			$html .= '</div>';
+		}
+		$html .= '</div>';
+	}
+
+	$html .= '</div>';
+	return $html;
+}
+add_shortcode( 'mu_livestream_live', 'mu_livestream_live' );
+
 /**
  * Shortcode to display upcoming livestreams
  *
@@ -32,13 +108,19 @@ function mu_livestream_upcoming( $atts, $content = null ) {
 			'post_type'      => 'mu-livestream',
 			'posts_per_page' => 12,
 			'meta_key'       => 'mu_livestream_start', // phpcs:ignore
-			'orderby'        => 'meta_value_num',
+			'orderby'        => 'meta_value',
 			'meta_type'      => 'DATETIME',
 			'order'          => 'ASC',
 			'meta_query'     => array( // phpcs:ignore
 				'relation' => 'AND',
 				array(
 					'key'     => 'mu_livestream_end',
+					'value'   => Carbon::now()->setTimezone( 'America/Detroit' )->format( 'Y-m-d H:i:s' ), // phpcs:ignore
+					'type'    => 'DATETIME',
+					'compare' => '<=',
+				),
+				array(
+					'key'     => 'mu_livestream_start',
 					'value'   => Carbon::now()->setTimezone( 'America/Detroit' )->format( 'Y-m-d H:i:s' ), // phpcs:ignore
 					'type'    => 'DATETIME',
 					'compare' => '>=',
@@ -101,10 +183,9 @@ function mu_livestream_past( $atts, $content = null ) {
 			'post_type'      => 'mu-livestream',
 			'posts_per_page' => 12,
 			'meta_key'       => 'mu_livestream_start', // phpcs:ignore
-			'orderby'        => array(
-				'meta_value' => 'mu_livestream_start', // phpcs:ignore
-				'menu_order' => 'DESC',
-			),
+			'orderby'        => 'meta_value',
+			'meta_type'      => 'DATETIME',
+			'order'          => 'DESC',
 			'meta_query'     => array( // phpcs:ignore
 				'reation' => 'AND',
 				array(
